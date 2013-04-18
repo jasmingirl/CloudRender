d81b3ccd4b3462aa6f1450068d7fe3c31ac2e1f0
jasmingirl <encomium_moriae@hotmail.com>
2013-04-18 16:59:58 +0900

    Update IsoSurface.h

--- IsoSurface.h
+++ IsoSurface.h
@@ -5,33 +5,31 @@
 #include <string>
 #include <sstream>
 using namespace std;
+#include <miffy/fileformat/ply.h>
 #include <miffy/gl/glutility.h>
-#include <miffy/fileformat/jverts.h>
 using namespace miffy;
 
 /// 等値面クラス。
 /*! データを読み込んで適切な色で表示するだけ。
     色もデータ化すべきよね。
-  VBOにしようとして失敗している。
+	VBOにしようとして失敗している。
 */
 class CIsoSurface {
 public:
-	CIsoSurface(float elerange,string& _dir);
+	CIsoSurface(float elerange);
 	~CIsoSurface(void);
-	JFormat* mSurfaceData[5];///< 等値面　とりあえず、jvertとjvert2両対応にした。
-	void Init();
+	int m_VertNum[5];
+	int m_IndexNum[5];
+	void Init(const char* _inifilepath);
 	bool Run();
 	void Info();
 	void ToggleMode(bool _showtext);
 private:
 	string m_title;
 	bool mEnlightCore;
-	string m_directory;///< いずれ消したいメンバ変数
 	float m_elerange;///< こいつもいずれ消したい
-	vector<GLuint> m_ListId;///< ディスプレイリストのID 使ってない。
-	GLuint m_BufferId;///< 今はこれ、頂点バッファで描いている。
-	GLuint m_IdBufferId;///< 頂点インデックスのバッファ
-	bool m_bJVert2;///< jvert2なのかどうか
+	Ply plydata[5];
+	color<unsigned char> m_Colors[5];
 };
 
 #endif
